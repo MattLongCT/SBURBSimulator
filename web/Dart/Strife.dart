395c9ec513812b53770e9b470e24df5dca5a543a
MattLongCT <37944261+MattLongCT@users.noreply.github.com>
2019-05-25 17:57:20 -0400

    +" JR: " to Comments

--- web/Dart/Strife.dart
+++ web/Dart/Strife.dart
@@ -1,20 +1,20 @@
 /*
 
-  Stubbing how i want to strife refactor to go.
-  strifes being all stupidly part of GameEntity was a decision I regretted early on
-  but i'm glad i waited till now to refactor it proper style.
+   JR: Stubbing how i want to strife refactor to go.
+   JR: strifes being all stupidly part of GameEntity was a decision I regretted early on
+   JR: but i'm glad i waited till now to refactor it proper style.
 
-  My expectation is for a strife to have at least 2 sides.  I don't expect to support MORE than 2
-  any time soon, but you are supposed to do "0, 1 or infinite" in your designs.
+   JR: My expectation is for a strife to have at least 2 sides.  I don't expect to support MORE than 2
+   JR: any time soon, but you are supposed to do "0, 1 or infinite" in your designs.
  */
 
 class Strife {
-  List<Team> teams; //at least 2
+  List<Team> teams; // JR: at least 2
 }
 
 class Team {
-  //i wanted this to be a list of Game Entities, but apparently that's not a thing??? can't use custom classes as generics? That doesn't sound right...
-  //yeah see, you can have a list<team> of teams up there. maybe the fact that game entity lives in a different file?
+  // JR: i wanted this to be a list of Game Entities, but apparently that's not a thing??? can't use custom classes as generics? That doesn't sound right...
+  // JR: yeah see, you can have a list<team> of teams up there. maybe the fact that game entity lives in a different file?
   List<Object> members;
-  String name; //'the players' 'the midnight crew'.  so sick of guessing if i should call something "the PLAYERS or not"
+  String name; // JR: 'the players' 'the midnight crew'.  so sick of guessing if i should call something "the PLAYERS or not"
 }
